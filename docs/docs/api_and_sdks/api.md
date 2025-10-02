c1c0d8f2c9bd4f91de0865104863a5d3dd2ea1ef
Jeff <jeff.zemerick@philterd.ai>
2025-10-02 13:15:14 -0400

    Consolidating code and simplifying.

--- docs/docs/api_and_sdks/api.md
+++ docs/docs/api_and_sdks/api.md
@@ -4,7 +4,6 @@ Philter's API is divided into three parts, the [Filtering API](api/filtering_api
 
 * [Filtering API](api/filtering_api.md) - The filtering API is used to redact text. With the filtering API, you can send text or PDF documents to Philter and receive back the redacted text or PDF document.
 * [Policies API](api/policies_api.md) - The policies API allows you to create, modify, and delete [policies](../policies/filter_policies.md). Policies can also be created manually with access to Philter, but the API provides a programmatic way to manage policies.
-* [Alerts API](api/alerts_api.md) - The alerts API allows you to get and delete [alerts](../other_features/alerts.md) that were generated during redaction.
 
 The Philter [SDKs](sdks.md) provide convenient methods for using Philter's API methods for various programming languages.
 
---
+++ philter-app/src/test/java/api/ApiControllerTests.java
@@ -0,0 +1,110 @@
+package api;
+
+import ai.philterd.phileas.PhileasConfiguration;
+import ai.philterd.phileas.model.enums.MimeType;
+import ai.philterd.phileas.model.objects.Explanation;
+import ai.philterd.phileas.model.objects.FilterResponse;
+import ai.philterd.phileas.policy.Policy;
+import ai.philterd.phileas.services.filters.FilterService;
+import ai.philterd.philter.api.controllers.FilterApiController;
+import ai.philterd.philter.api.controllers.StatusApiController;
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Properties;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Ignore
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class ApiControllerTests {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private FilterService filterService;
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    @Configuration
+    @EnableWebMvc
+    static class ContextConfiguration {
+
+        @Bean
+        public FilterApiController filterApiController() {
+            return new FilterApiController(filterService());
+        }
+
+        @Bean
+        public StatusApiController statusApiControllerApiController() throws IOException {
+            return new StatusApiController(phileasConfiguration());
+        }
+
+        @Bean
+        public FilterService filterService() {
+            return Mockito.mock(FilterService.class);
+        }
+
+        @Bean
+        public Gson gson() {
+            return new Gson();
+        }
+
+        @Bean
+        public PhileasConfiguration phileasConfiguration() {
+            return new PhileasConfiguration(new Properties());
+        }
+
+    }
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+    @Test
+    public void filterTestNoBody() throws Exception {
+
+        mockMvc.perform(post("/api/filter").contentType("text/plain").accept("text/plain")).andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    @Ignore("Throwing an NPE")
+    public void filterTest() throws Exception {
+
+        final Policy policy = new Policy();
+        when(filterService.filter(policy, "George Washington was president.", "none", "documentid", MimeType.TEXT_PLAIN)).thenReturn(new FilterResponse("*** was president.", "none", "none", 0, new Explanation(Collections.emptyList(), Collections.emptyList()), Collections.emptyMap(), Collections.emptyList(), 0));
+
+        mockMvc.perform(post("/api/filter").content("George Washington was president.").param("c", "none").param("d", "none").contentType("text/plain")).andExpect(status().isOk());
+
+    }
+
+    @Test()
+    public void statusTest() throws Exception {
+
+        mockMvc.perform(get("/api/status")).andExpect(status().isServiceUnavailable());
+
+    }
+
+}

7638c1e1da8c19296dd2074fd48422dbf679d16e
jzonthemtn <jeff.zemerick@philterd.ai>
2026-01-20 15:40:42 -0500

    Undoing changes to script.

--- distribution/example-filter.sh
+++ distribution/example-filter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl  "http://localhost:8080/api/filter" \
+curl -k "https://localhost:8080/api/filter" \
 	--data "George Washington was president and his ssn was 123-45-6789 and he lived in 90210." -H "Content-type: text/plain"
